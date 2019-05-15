d09af8280bf9ac806837d2d9f63606f5187cba3e
Oleksandr <32979603+AlexanderHubar@users.noreply.github.com>
2019-05-15 13:03:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@ const wrapperMenu = document.querySelector('.wrapper-menu');
 const mobileNav = document.querySelector('.mobile-nav');
 const header =  document.querySelector('.header');
 const welcomeBtn = document.querySelector('.welcome-section__scroll');
-const experienceSection = document.querySelector('.expirience')
-const scrollBtn = document.querySelector('.scroll-top')
-const screen = window.innerHeight
+const experienceSection = document.querySelector('.expirience');
+const scrollBtn = document.querySelector('.scroll-top');
+const screen = window.innerHeight;
 
 wrapperMenu.addEventListener('click',() => {
 	wrapperMenu.classList.toggle('open'); 
 	mobileNav.classList.toggle('visible');
-})
+});
 
 window.addEventListener('scroll', function(e) {
 
 	let scrollPosition = window.scrollY;
 
 	if (scrollPosition > 30) {
-		header.style.background = '#5097ff'
-		header.style.padding = '5px 0'
+		header.style.background = '#5097ff';
+		header.style.padding = '5px 0';
 	} else {
-		header.style.background = 'transparent'
-		header.style.padding = '14px 0'
+		header.style.background = 'transparent';
+		header.style.padding = '14px 0';
 	}
   
 });
@@ -32,18 +32,15 @@ welcomeBtn.addEventListener('click',() => {
         left: 0,
         behavior: 'smooth'
     });
-})
+});
 
 window.addEventListener('scroll', () => {
-
 	let scrollPosition = window.scrollY;
-
 	if (scrollPosition > screen) {
 		scrollBtn.style.display = 'block'
 	} else {
 		scrollBtn.style.display = 'none'
 	}
-
 });
 
 scrollBtn.addEventListener('click',() => {
@@ -52,4 +49,4 @@ scrollBtn.addEventListener('click',() => {
 		left: 0, 
 		behavior: 'smooth'
 	});
-})
+});
